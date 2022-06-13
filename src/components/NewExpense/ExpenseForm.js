d2edd5e39362cea19b05008e8e2a99f6cbe290b6
samith thilanka <samith@rhino-partners.com>
2022-06-13 11:42:45 +0530

    expense form

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -6,7 +6,28 @@ const ExpenseForm = (props) => {
     const [enteredAmount,setEnteredAmount] = useState('');
     const [enteredDate,seEnteredDate] = useState('');
 
+    //use only one state instead of using three state slices
+    // const [userInputs,setUserInputs] = useState({
+    //     enteredTitle:'',
+    //     enteredAmount:'',
+    //     enteredDate:''
+    // });
+
     const titleChangeHandler = (event) => {
+        // setUserInputs({
+        //     ...userInputs,
+        //     enteredTitle:event.target.value
+        // });
+
+        //above is not a good practice because if we are depending on the previous state, use this method
+
+        // setUserInputs((prevState) => { //safer way
+        //     return {
+        //         ...prevState, enteredTitle: event.target.value
+        //     };
+        // });
+
+        //use only one state instead of using three state slices
         setEnteredTitle(event.target.value);
     }  
     const amountChangeHandler = (event) => {
@@ -15,21 +36,39 @@ const ExpenseForm = (props) => {
     const dateChangeHandler = (event) => {
         seEnteredDate(event.target.value);
     }  
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+
+        const expenseData = {
+            title: enteredTitle,
+            amount: enteredAmount,
+            date: new Date(enteredDate)
+        }
+
+        //two way binding
+        setEnteredTitle('');
+        setEnteredAmount('');
+        seEnteredDate('');
+        console.warn(expenseData);
+
+
+    }
     
     return (
-        <form>
+        <form onSubmit={submitHandler}>
             <div className='new-expense__controls'>
                 <div className='new-expense__control'>
                     <label>Title</label>
-                    <input type="text"  onChange={titleChangeHandler}/>
+                    <input type="text" value={enteredTitle}  onChange={titleChangeHandler}/>
                 </div>
                 <div className='new-expense__control'>
                     <label>Amount</label>
-                    <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler} />
+                    <input type="number" value={enteredAmount} min="0.01" step="0.01" onChange={amountChangeHandler} />
                 </div>
                 <div className='new-expense__control'>
                     <label>Date</label>
-                    <input type="date" min="2022-01-01" max="2022.12.30"  onChange={dateChangeHandler}/>
+                    <input type="date" value={enteredDate} min="2022-01-01" max="2022.12.30"  onChange={dateChangeHandler}/>
                 </div>
             </div>
             <div className='new-expense__actions'>
