257990d94cad70b352749b447ba3f3e98c751d97
samithperera94 <samiththilanka94@gmail.com>
2022-06-15 06:55:26 +0530

    dynamically load new expense form

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -3,17 +3,34 @@ import './NewExpense.css';
 import ExpenseForm from './ExpenseForm';
 
 const NewExpense = (props) => {
+
+    const [showForm, setShowForm] = useState(false)
     const saveExpenseDataHandler = (enteredExpenseData) => {
         const expenseData = {
             ...enteredExpenseData,
             id: Math.random().toString()
         };
-        // console.log('::::', expenseData);
-        props.onAddExpense(expenseData)
+        props.onAddExpense(expenseData);
+        setShowForm(false)
     };
+
+    const addNewExpenseHandler = () => {
+
+        // setShowForm((prevState) => {
+        //     return !prevState;
+        // });
+        setShowForm(true)
+    };
+
+    const cancelFormHandler = () => {
+        setShowForm(false)
+    }
     return (
         <div className='new-expense'>
-            <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+
+            {(showForm == true) ? <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} onCancelForm={cancelFormHandler} /> :
+                <button className="new-expense__add-new" onClick={addNewExpenseHandler}>Add New Expense</button>}
+
         </div>
     );
 }
